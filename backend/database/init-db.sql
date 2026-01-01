a4d5ccc2556364b95b27843acca1d443fa957b2b
Ethan Le <ethanisverybusyrightnow@gmail.com>
2025-12-31 21:25:31 -0800

    created dockerfile for db and deleted fastapi server

---
+++ backend/database/init-db.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS mangas;
+
+CREATE TABLE mangas(
+    title VARCHAR(255) PRIMARY KEY,
+)
+
+INSERT INTO mangas (title) VALUES ('One Piece');
+INSERT INTO mangas (title) VALUES ('Naruto');
