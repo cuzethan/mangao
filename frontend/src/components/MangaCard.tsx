ec8c02925e311a50dd72f5f11e62ef33010f9d1c
Ethan Le <ethanisverybusyrightnow@gmail.com>
2025-12-31 19:10:29 -0800

    modifed img and font for mangacard

--- frontend/src/components/MangaCard.tsx
+++ frontend/src/components/MangaCard.tsx
@@ -11,11 +11,11 @@ function MangaCard({name, status, imgUrl}: CardProps) {
         <div className="border-white border-2 rounded-md p-4 flex justify-between">
             <div className="flex flex-col gap-2">
                 <h2 className="font-bbh text-3xl">{name}</h2>
-                <img src={imgUrl}/>
+                <img className="w-32 h-48 object-cover" src={imgUrl}/>
             </div>
             <div className="flex flex-col justify-end font-nunito text-xl">
                 <p>
-                    <span className="font-nunito-bold">Name:</span> {status}
+                    <span className="font-nunito-bold">Status:</span> {status}
                 </p>
             </div>
         </div>
