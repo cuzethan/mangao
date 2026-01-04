d327c6a74bed2e9c149cdf8938e15290c8acb69e
Ethan Le <ethanisverybusyrightnow@gmail.com>
2026-01-03 19:29:32 -0800

    added pg dep. and fixed sql startup

--- backend/database/init-db.sql
+++ backend/database/init-db.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS mangas;
 
 CREATE TABLE mangas(
-    title VARCHAR(255) PRIMARY KEY,
-)
+    title VARCHAR(255) PRIMARY KEY
+);
 
 INSERT INTO mangas (title) VALUES ('One Piece');
 INSERT INTO mangas (title) VALUES ('Naruto');
