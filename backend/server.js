89df7aa0aa66a35b892f9ff14349d34ca9c725fb
cuzethan <leethan444@gmail.com>
2026-01-06 14:52:03 -0800

    altered docker-compose so server can read from docker db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
-const express = require('express')
+import express from 'express'
+import { Client } from 'pg'
+
 const app = express()
 const port = process.env.PORT || 3000;
 
+const client = new Client()
+await client.connect()
+ 
+
 app.get('/', (req, res) => {
-    res.send("DOCKER IFNLALY WORKS XDDD")
+    res.send("DOCKER IFNLAL TESTING TESTING")
+})
+
+app.get('/testing', async (req, res) => {
+    const query = 'SELECT * FROM mangas'
+    const result = await client.query(query)
+    res.send(result)
 })
 
 app.listen(port, () => {
