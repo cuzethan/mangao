7e891979576c1f389d708617d07b30e51c6736db
Ethan Le <ethanisverybusyrightnow@gmail.com>
2026-01-15 07:53:10 -0800

    created backend connection to addManga w/ simple validation

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cors from 'cors'
 
 const app = express()
 const port = process.env.VITE_BACKEND_PORT || 3000;
+
+app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:5173', // Replace with your actual Vite port
     methods: ['GET', 'POST'],
@@ -22,6 +24,34 @@ app.get('/getMangaList', async (req, res) => {
         const query = 'SELECT * FROM mangas';
         const result = await client.query(query);
         res.json(result.rows); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+app.post('/addManga', async (req, res) => {
+    const { title, status, imageurl } = req.body;
+    console.log(title, status, imageurl)
+    if (!title) {
+        return res.status(400).json({
+            error: "Invalid Title",
+            message: "Make sure you input a title."
+        })
+    }
+
+    if (imageurl && !imageurl.startsWith('http')) {
+        return res.status(400).json({ 
+            error: "Invalid URL", 
+            message: "Please provide a valid image link." 
+        });
+    }
+
+    try {
+        const query = "INSERT INTO mangas (title, status, imageurl) VALUES ($1, $2, $3)";
+        const values = [title, status, imageurl || null]
+        await client.query(query, values)
+        res.status(201).send(`Recieved the data!`)
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
--- frontend/src/components/AddMangaForm.tsx
+++ frontend/src/components/AddMangaForm.tsx
@@ -1,18 +1,29 @@
+import axios from 'axios'
+
+const url = `http://localhost:${import.meta.env.VITE_PORT|| 3000}`
+
 interface AddMangaFormProps {
     closeModal: () => void
 }
 
+
 export default function AddMangaForm({closeModal}: AddMangaFormProps) {
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        console.log("Works")
+        const formData = new FormData(e.currentTarget)
+        const data = Object.fromEntries(formData.entries())
+
+        axios.post(`${url}/addManga`, data)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+
         closeModal()
     }
-    
+
     return (
         <form method="post" onSubmit={handleSubmit} className="text-black flex flex-col gap-2">
             <label>
-                Title:  <input name="titleInput" className="border p-1 rounded-lg w-7/8"/>
+                Title:  <input name="title" className="border p-1 rounded-lg w-7/8"/>
             </label>
             <label>
                 Status: <select name="status" className="border p-1 rounded-lg">
@@ -23,9 +34,10 @@ export default function AddMangaForm({closeModal}: AddMangaFormProps) {
                 </select>
             </label>
             <label>
-                Image URL: <input name="imgURLInput" className="border p-1 rounded-lg w-60"/>
+                Image URL: <input name="imageurl" className="border p-1 rounded-lg w-60"
+                placeholder="Leave empty for default..."/>
             </label>
-            <button type="submit" className="border p-1 rounded-lg">Submit Form</button>
+            <button type="submit" className="border p-1 rounded-lg hover:bg-black/5">Submit Form</button>
         </form>
     )
 }
