2f204361e5644515ff55cb4db59157b1604f1b05
Ethan Le <ethanisverybusyrightnow@gmail.com>
2026-01-28 15:50:24 -0800

    added users db

--- database/init-db.sql
+++ database/init-db.sql
@@ -1,11 +1,18 @@
 DROP TABLE IF EXISTS mangas;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password_hashed VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE mangas (
-    title VARCHAR(255) PRIMARY KEY,
-    status VARCHAR(255),
+    title VARCHAR(255) PRIMARY KEY NOT NULL,
+    status VARCHAR(255) NOT NULL,
     imageURL TEXT
 );
 
 INSERT INTO mangas VALUES ('One Piece', 'completed', NULL);
-INSERT INTO mangas VALUES ('Naruto', 'unfinished', NULL);
+INSERT INTO mangas VALUES ('Naruto', 'planned', NULL);
 INSERT INTO mangas VALUES ('Bleach', 'planned', NULL);
